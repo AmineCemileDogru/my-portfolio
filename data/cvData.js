666103b10aed72926e268e7e471ab744b66d5d66
Deyto7 <115898497+Deyto7@users.noreply.github.com>
2026-06-05 15:13:30 +0300

    Change LinkedIn button to direct login page

--- data/cvData.js
+++ data/cvData.js
@@ -4,7 +4,7 @@ export const cvData = {
     title: "Bilgisayar Mühendisi",
     location: "Kocaeli, Gebze",
     email: "aminecemiledogru@gmail.com",
-    linkedin: "https://www.linkedin.com/in/amine-cemile-doğru-04416a255?utm_source=share_via&utm_content=profile&utm_medium=member_ios",
+    linkedin: "https://www.linkedin.com/login",
     photo: "/profile.jpg",
     born: 2004,
     graduated: 2026,
